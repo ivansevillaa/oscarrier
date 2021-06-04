3ab59d8a96393862dc6e4e80046bc79d047bca59
Ivan Sevilla <ivansevillaa@gmail.com>
2021-06-04 20:32:15 -0300

    Added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Chakra from "@components/Chakra";
 import Layout from "@components/Layout";
+import { pageview } from "@utils/gtag";
+
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Chakra cookies={pageProps.cookies}>
       <Layout>
