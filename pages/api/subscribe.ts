62762025a66b01ccc1df1132026287e70c6245d9
Ivan Sevilla <ivansevillaa@gmail.com>
2021-08-03 20:15:13 -0300

    add newsletter

---
+++ pages/api/subscribe.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
+  try {
+    const API_KEY = process.env.BUTTONDOWN_API_KEY;
+
+    const response = await fetch(
+      "https://api.buttondown.email/v1/subscribers",
+      {
+        body: JSON.stringify({ email }),
+        headers: {
+          Authorization: `Token ${API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    );
+
+    if (response.status >= 400) {
+      return res.status(response.status).json({
+        error: await response.text(),
+        errCode: "ERR-40X"
+      });
+    }
+
+    return res.status(201).json({ error: "", errCode: "" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString(), errCode: "ERR-50X" });
+  }
+};
