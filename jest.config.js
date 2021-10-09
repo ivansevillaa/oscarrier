64ccd5bc23675d1e0278d5b3c9d4233dcfadbbe2
Ivan Sevilla <ivansevillaa@gmail.com>
2021-10-09 18:05:04 -0300

    add example to support custom module resolution with jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
-  snapshotSerializers: ["@emotion/jest/serializer"]
+  snapshotSerializers: ["@emotion/jest/serializer"],
+  moduleNameMapper: {
+    // TODO: complete the other aliases of the project
+    "@components/(.*)": "<rootDir>/components/$1"
+  }
 };
