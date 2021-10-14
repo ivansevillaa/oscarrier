9130b22efe6a6ddf8678ac95ebe13f40c4d8ff6c
Ivan Sevilla <ivansevillaa@gmail.com>
2021-10-13 22:14:29 -0300

    added coverage config, watch and debug mode

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,11 @@ module.exports = {
   moduleNameMapper: {
     // TODO: complete the other aliases of the project
     "@components/(.*)": "<rootDir>/components/$1"
-  }
+  },
+  collectCoverageFrom: [
+    "components/**/*.{ts,tsx}",
+    "pages/**/*.{ts,tsx}",
+    "utils/**/*.ts"
+  ]
+  // TODO: add coverage threshold
 };
