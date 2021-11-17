f968b2c611b5a846870eacd7fb87321965f84e67
Ivan Sevilla <ivansevillaa@gmail.com>
2021-11-16 22:00:07 -0300

    change script tag to add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import {useEffect} from "react";
+import Script from "next/script";
 import {useRouter} from "next/router";
 import Chakra from "@components/Chakra";
-import {pageview} from "@utils/gtag";
+import {pageview, GA_TRACKING_ID} from "@utils/gtag";
 
 import type {AppProps} from "next/app";
 
@@ -21,8 +22,28 @@ export default function MyApp({Component, pageProps}: AppProps): JSX.Element {
   }, [router.events]);
 
   return (
-    <Chakra cookies={pageProps.cookies}>
-      <Component {...pageProps} />
-    </Chakra>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `
+        }}
+      />
+      <Chakra cookies={pageProps.cookies}>
+        <Component {...pageProps} />
+      </Chakra>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ import Document, {
   DocumentContext,
   DocumentInitialProps
 } from "next/document";
-import {GA_TRACKING_ID} from "@utils/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(
@@ -21,23 +20,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/images/icon.png" />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-            }}
-          />
         </Head>
         <body>
           <Main />
